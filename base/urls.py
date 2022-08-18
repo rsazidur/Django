608ac926867573b34c8a3af3ab98770e89e3757c
Molla Sazidur Rahman <rsazidur936@gmail.com>
2022-08-18 13:17:11 +0600

    Created a new url in base for apps. Added few function in views and import a module

---
+++ base/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="Home Page"),
+    path('room/', views.room, name="Room"),
+
+]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('Home Page')
+
+def room(request):
+    return HttpResponse('Room')
+
 
 # Create your views here.
