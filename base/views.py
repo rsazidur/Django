59ee54097a05a240022b8cb9a900abc7712efe33
Molla Sazidur Rahman <rsazidur936@gmail.com>
2022-09-08 16:33:02 +0600

    Updated the views

--- base/views.py
+++ base/views.py
@@ -5,10 +5,8 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Home Page')
+    return render(request, 'home.html')
+
 
 def room(request):
-    return HttpResponse('Room')
-
-
-
+    return render(request, 'room.html')
